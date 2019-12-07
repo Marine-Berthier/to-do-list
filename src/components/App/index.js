bef43d35344363bea8d72a940e42f9312d7beef1
Marine Berthier <marine154@hotmail.fr>
2019-12-07 08:52:35 +0100

    state WIP

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -11,12 +11,29 @@ import Main from 'src/components/Main';
 import tasks from 'src/data/tasks';
 
 // == Composant
-const App = () => (
+class App extends React.Component {
+
+  constructor(props) {
+    // On appelle le constructor du parent React.Component
+    super(props);
+    // State : stocker l'état de l'application
+    // Si le state change, le render() du composant est relancé
+    this.state = {
+      done: false,
+    };
+    console.log(this);
+  }
+
+render(){
+
+  return(
   <div id="app">
     <Form />
     <Main tasks={tasks} />
   </div>
-);
+  );
+  }
+}
 
 // == Export
 export default App;
